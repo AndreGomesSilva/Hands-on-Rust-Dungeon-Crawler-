b3f2cad9511e9a3fc8203321b7624dfb098c645d
AndreGomesSilva <silva_andregomes@outlook.com>
2026-06-23 23:36:11 -0300

    repo init: basic game with map render, player, monster and combat collision

---
+++ src/systems/player_input.rs
@@ -0,0 +1,68 @@
+use legion::systems::CommandBuffer;
+
+use crate::prelude::*;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+pub fn player_input(
+    ecs: &mut SubWorld,
+    commands: &mut CommandBuffer,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] turn_state: &mut TurnState,
+) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::A => Point::new(-1, 0),
+            VirtualKeyCode::D => Point::new(1, 0),
+            VirtualKeyCode::W => Point::new(0, -1),
+            VirtualKeyCode::S => Point::new(0, 1),
+            _ => Point::new(0, 0),
+        };
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        if delta.x != 0 || delta.y != 0 {
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: player_entity,
+                            victim: *entity,
+                        },
+                    ));
+                });
+            if !hit_something {
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: player_entity,
+                        destination,
+                    },
+                ));
+            }
+        }
+        *turn_state = TurnState::PlayerTurn;
+
+        // players.iter(ecs).for_each(|(entity, pos)| {
+        //     let destination = *pos + delta;
+        //     commands.push((
+        //         (),
+        //         WantsToMove {
+        //             entity: *entity,
+        //             destination,
+        //         },
+        //     ));
+        // });
+        // *turn_state = TurnState::PlayerTurn;
+    }
+}
